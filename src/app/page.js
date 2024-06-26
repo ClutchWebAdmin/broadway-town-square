76cafacbdd2244fadf6b4f0a43f3b344f89eb778
Gabe Wolford <gabewolford@gmail.com>
2024-06-26 12:54:33 -0700

    decreased footer text size, added meta tags, og images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,32 @@
 import Hero from "./components/Hero";
 
+export const metadata = {
+  title: "Broadway Town Square Apartments | 1127 Broadway St E",
+  description: "1127 Broadway St E, Salem, Oregon, 97301",
+  keywords:
+    "salem oregon apartments, downtown salem apartments, downtown salem living, 2 bedroom apartments salem oregon, 2 bedroom 2 bath apartments downtown salem",
+  openGraph: {
+    title: "Broadway Town Square Apartments | 1127 Broadway St E",
+    description: "1127 Broadway St E, Salem, Oregon, 97301",
+    siteName: "Broadway Town Square Apartments",
+    type: "website",
+    locale: "en_US",
+    url: "https://broadwaytownsquare.com",
+    images: [
+      {
+        url: "https://broadwaytownsquare.com/images/og-image.png",
+        alt: "Broadway Town Square Apartments",
+      },
+    ],
+  },
+  images: [
+    {
+      url: "https://broadwaytownsquare.com/images/og-image.png",
+      alt: "Broadway Town Square Apartments",
+    },
+  ],
+};
+
 export default function HomePage() {
   return (
     <main>
--- src/app/photos/page.js
+++ src/app/photos/page.js
@@ -1,3 +1,30 @@
+export const metadata = {
+  title: "Photos | Broadway Town Square Apartments",
+  description: "1127 Broadway St E, Salem, Oregon, 97301",
+  keywords:
+    "salem oregon apartments, downtown salem apartments, downtown salem living, 2 bedroom apartments salem oregon, 2 bedroom 2 bath apartments downtown salem",
+  openGraph: {
+    title: "Photos | Broadway Town Square Apartments",
+    description: "1127 Broadway St E, Salem, Oregon, 97301",
+    siteName: "Broadway Town Square Apartments",
+    type: "website",
+    locale: "en_US",
+    url: "https://broadwaytownsquare.com",
+    images: [
+      {
+        url: "https://broadwaytownsquare.com/images/og-image.png",
+        alt: "Broadway Town Square Apartments",
+      },
+    ],
+  },
+  images: [
+    {
+      url: "https://broadwaytownsquare.com/images/og-image.png",
+      alt: "Broadway Town Square Apartments",
+    },
+  ],
+};
+
 export default function PhotosPage() {
   return (
     <main className="mt-[var(--headerHeightMobile)] lg:mt-[var(--headerHeightDesktop)] px-5">
